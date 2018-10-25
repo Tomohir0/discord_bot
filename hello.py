fad783813fd54c1900d97b91f5fac290cb259240
Tomohiro <nakamotog@gmail.com>
2018-10-26 04:50:02 +0900

    make it better

--- hello.py
+++ hello.py
@@ -11,13 +11,14 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-# 「おはよう」で始まるか調べる
- if message.content.startswith("おはよう"):
+# 「こん」で始まるか調べる
+ if message.content.startswith("こん"):
 # 送り主がBotだった場合反応したくないので
   if client.user != message.author:
  # メッセージを書きます
-   m = "おはようございます" + message.author.name + "さん！/r/n投票お忘れなく！"
+   m = "おはようございます" + message.author.name + "さん！投票お忘れなく！"
 # メッセージが送られてきたチャンネルへメッセージを送ります
    await client.send_message(message.channel, m)
 
+   
 client.run("NTA1MDE0MDQyNzM4Mjk0Nzg1.DrNeBA.dvNe0cvuyZAi9BZPumN8hXCVzzE")
