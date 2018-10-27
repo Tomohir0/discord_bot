f1ee4fe028ee8725594f29b9d2ad853baf18c677
Tomohiro <nakamotog@gmail.com>
2018-10-27 15:09:08 +0900

    make it better

--- hello.py
+++ hello.py
@@ -1,13 +1,15 @@
 import discord
+import time
 
+now_time = time.time()
 client = discord.Client()
 
 @client.event
 async def on_ready():
- print('Logged in as')
- print(client.user.name)
- print(client.user.id)
- print('------')
+  print('Logged in as')
+  print(client.user.name)
+  print(client.user.id)
+  print('------')
 
 @client.event
 async def on_message(message):
@@ -15,10 +17,16 @@ async def on_message(message):
  if message.content.startswith("こん"):
 # 送り主がBotだった場合反応したくないので
   if client.user != message.author:
- # メッセージを書きます
-   m = "おはようございます" + message.author.name + "さん！投票お忘れなく！"
+# メッセージを書きます
+#   m = "Hello, ほも" + message.author.name + ""
+   m = "Hi, " + message.author.name + "投票し忘れてるでゲソ"
 # メッセージが送られてきたチャンネルへメッセージを送ります
    await client.send_message(message.channel, m)
-
+ elif message.content.startswith("おはよう"):
+  if client.user != message.author:
+# メッセージを書きます
+   m = "Good morning, " + message.author.name + "投票し忘れてるでゲソ"
+# メッセージが送られてきたチャンネルへメッセージを送ります
+   await client.send_message(message.channel, m)
    
 client.run("NTA1MDE0MDQyNzM4Mjk0Nzg1.DrNeBA.dvNe0cvuyZAi9BZPumN8hXCVzzE")
