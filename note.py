8c1561e98e38231d041fcd101e4d7cd5403f4dd3
Tomohiro <nakamotog@gmail.com>
2018-11-02 04:10:32 +0900

    fix

--- note.py
+++ note.py
@@ -5,11 +5,10 @@ import os
 import pickle
 import random
 
-##import boto3
+#import boto3
 #bucket_name = "tomo-discord"
 #s3 = boto3.resource('s3')
 ## s3連携
-
 
 
 class Note():
@@ -41,7 +40,7 @@ class Note():
         else:
             with open(f_name, 'rb') as f:
                 memos = pickle.load(f)
-                await self.bot.say(memos.get(label, label + "なんてlabelのメモないよ！"))
+            await self.bot.say(memos.get(label, label + "なんてlabelのメモないよ！"))
 
     @commands.command(description=' ', pass_context=True,)
     async def labels(self, ctx: commands.Context):
@@ -52,10 +51,10 @@ class Note():
         else:
             with open(f_name, 'rb') as f:
                 memos = pickle.load(f)
-                await self.bot.say("ここのmemoのlabel一覧は\n")
-                for label in memos.keys():
-                    await self.bot.say(label)
-                await self.bot.say("\nだよ！")
+            await self.bot.say("ここのmemoのlabel一覧は\n")
+            for label in memos.keys():
+                await self.bot.say(label)
+            await self.bot.say("\nだよ！")
 
     @commands.command(description='「?notes」で保存されたmemoからランダムに一つを晒します。', pass_context=True)
     async def callrand(self, ctx: commands.Context):
@@ -67,6 +66,25 @@ class Note():
             with open(f_name, 'rb') as f:
                 memos = pickle.load(f)
             label_selected = random.choice(list(memos.keys()))
-            await self.bot.say(label_selected +"\n"+ memos.get(label_selected, "ERROR"))
+            await self.bot.say(label_selected +" : "+ memos.get(label_selected, "ERROR"))
+
+    @commands.command(description='「?notes」で保存されたmemoを読み出すことができます。', pass_context=True)
+    async def deletes(self, ctx: commands.Context, label: str):
+        "「?delete secret」でsecretとして保存されたメモを削除できる……かも。"
+        f_name = "/tmp/memos_" + ctx.message.author.server.id + ".pkl"
+        if not os.path.isfile(f_name):  # 存在しないときの処理
+            await self.bot.say("まだこのserverにはメモがないよ……。?notesを使ってほしいな……")
+        else:
+            with open(f_name, 'rb') as f:
+                memos = pickle.load(f)
+            if not label in memos:
+                await self.bot.say(label + "なんてラベルないよ！「?labels」使う？")
+            elif random.randint(1, 100) > 90:
+                await self.bot.say(label+"の「"+memos.pop(label)+"」は消えちゃった……")
+            else:
+                await self.bot.say(label + "の「" + memos.get(label) + "」は消えちゃった……")
+                await self.bot.say("\nと思いきや復活！！神！")
+                await self.bot.say(label + " : " + memos.get(label))
+
 def setup(bot):
     bot.add_cog(Note(bot))
